0183f22c59469abf98bf419628611df9356bcf77
zaryab ali <zaryab.ali@clarisync.com>
2025-04-05 17:47:34 +0500

    first commit

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
         <h1 className="font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">
           Read the most interesting articles
         </h1>
-        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
+        {/* <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
-        </p>
+        </p> */}
         <Search className="mt-10 lg:mt-6 xl:mt-10" />
         <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
           <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
